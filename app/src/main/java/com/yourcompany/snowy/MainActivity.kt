040c466e3d78fa170a0dce71b4d679ad9eba5d2a
slowpoke <amina.muradalieva91@gmail.com>
2024-01-25 15:53:05 +0600

    Starter code. No coroutines yet, only view pager and a bitmap filter

---
+++ app/src/main/java/com/yourcompany/snowy/MainActivity.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2023 Kodeco Inc.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ *  distribute, sublicense, create a derivative work, and/or sell copies of the
+ *  Software in any work that is designed, intended, or marketed for pedagogical or
+ *  instructional purposes related to programming, coding, application development,
+ *  or information technology.  Permission for such use, copying, modification,
+ *  merger, publication, distribution, sublicensing, creation of derivative works,
+ *  or sale is expressly withheld.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+package com.yourcompany.snowy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.material.tabs.TabLayoutMediator
+import com.yourcompany.snowy.databinding.ActivityMainBinding
+import com.yourcompany.snowy.model.Tutorial
+
+class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
+  private val tutorialPagerAdapter by lazy {
+    TutorialPagerAdapter(getTutorialData(), this)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    with(binding) {
+      viewPager.adapter = tutorialPagerAdapter
+      TabLayoutMediator(tabLayout, viewPager) { tab, position ->/* no-op */
+        tab.text = when (position) {
+          0 -> getString(R.string.kotlin_title)
+          1 -> getString(R.string.android_name)
+          2 -> getString(R.string.rxkotlin_name)
+          3 -> getString(R.string.kitura_name)
+          else -> throw IllegalArgumentException("Illegal position argument: $position")
+        }
+      }.attach()
+    }
+  }
+
+  private fun getTutorialData(): List<Tutorial> {
+    val tutorialList = arrayListOf<Tutorial>()
+    tutorialList.add(
+      Tutorial(
+        getString(R.string.kotlin_title),
+        getString(R.string.kotlin_url),
+        "",
+        getString(R.string.kotlin_desc)
+      )
+    )
+    tutorialList.add(
+      Tutorial(
+        getString(R.string.android_name),
+        getString(R.string.android_url),
+        getString(R.string.android_url),
+        getString(R.string.android_desc)
+      )
+    )
+    tutorialList.add(
+      Tutorial(
+        getString(R.string.rxkotlin_name),
+        getString(R.string.rxkotlin_url),
+        getString(R.string.rxkotlin_url),
+        getString(R.string.rxkotlin_desc)
+      )
+    )
+    tutorialList.add(
+      Tutorial(
+        getString(R.string.kitura_name),
+        getString(R.string.kitura_url),
+        getString(R.string.kitura_url),
+        getString(R.string.kitura_desc)
+      )
+    )
+    return tutorialList
+  }
+}
---
+++ app/src/main/java/com/yourcompany/snowy/TutorialFragment.kt
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2023 Kodeco Inc.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ *  distribute, sublicense, create a derivative work, and/or sell copies of the
+ *  Software in any work that is designed, intended, or marketed for pedagogical or
+ *  instructional purposes related to programming, coding, application development,
+ *  or information technology.  Permission for such use, copying, modification,
+ *  merger, publication, distribution, sublicensing, creation of derivative works,
+ *  or sale is expressly withheld.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+package com.yourcompany.snowy
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.yourcompany.snowy.databinding.FragmentTutorialBinding
+import com.yourcompany.snowy.model.Tutorial
+import com.yourcompany.snowy.utils.SnowFilter
+import kotlinx.coroutines.*
+import java.net.URL
+
+class TutorialFragment : Fragment(R.layout.fragment_tutorial) {
+  private var binding: FragmentTutorialBinding? = null
+
+  // TODO: Insert coroutineExceptionHandler
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    binding = FragmentTutorialBinding.bind(view)
+    val tutorial = arguments?.getParcelable<Tutorial>(TUTORIAL_KEY) as Tutorial
+    binding?.let {
+      with(it) {
+        tutorialName.text = tutorial.name
+        tutorialDesc.text = tutorial.description
+        reload.setOnClickListener { downloadTutorialImages(tutorial) }
+      }
+    }
+    downloadTutorialImages(tutorial)
+  }
+
+  private fun downloadTutorialImages(tutorial: Tutorial) {
+    if (tutorial.imageTwoUrl.isBlank()) {
+      downloadSingleImage(tutorial)
+    } else {
+      downloadTwoImages(tutorial)
+    }
+  }
+
+  private fun downloadSingleImage(tutorial: Tutorial) {
+    // TODO: Not implemented
+  }
+
+  private fun downloadTwoImages(tutorial: Tutorial) {
+    // TODO: Not implemented
+  }
+
+  //1
+  private suspend fun getOriginalBitmap(tutorial: Tutorial): Bitmap =
+    //2
+    withContext(Dispatchers.IO) {
+      //3
+      URL(tutorial.imageUrl).openStream().use {
+        return@withContext BitmapFactory.decodeStream(it)
+      }
+    }
+
+  private fun loadSnowFilter(originalBitmap: Bitmap): Bitmap {
+    return SnowFilter.applySnowEffect(originalBitmap)
+  }
+
+  private fun loadImage(snowFilterBitmap: Bitmap) {
+    binding?.let {
+      with(it) {
+        progressBar.visibility = View.GONE
+        reload.isVisible = false
+        errorMessage.isVisible = false
+        snowFilterImage.setImageBitmap(snowFilterBitmap)
+      }
+    }
+  }
+
+  private fun loadTwoImages(imageOne: Bitmap, imageTwo: Bitmap) {
+    binding?.let {
+      with(it) {
+        progressBar.visibility = View.GONE
+        reload.isVisible = false
+        errorMessage.isVisible = false
+        snowFilterImage.setImageBitmap(imageOne)
+        snowFilterSecondImage.setImageBitmap(imageTwo)
+      }
+    }
+  }
+
+  private fun showError(message: String) {
+    binding?.let {
+      with(it) {
+        errorMessage.isVisible = true
+        reload.isVisible = true
+        errorMessage.text = message
+        progressBar.isVisible = false
+      }
+    }
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    binding = null
+  }
+
+  companion object {
+    const val TUTORIAL_KEY = "TUTORIAL"
+
+    fun newInstance(tutorial: Tutorial): TutorialFragment {
+      return TutorialFragment().apply {
+        arguments = Bundle().apply {
+          putParcelable(TUTORIAL_KEY, tutorial)
+        }
+      }
+    }
+  }
+}
